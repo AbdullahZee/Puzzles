4981e470dd3e17364a4ca3b95b0f7f78014d8b2a
AbdullahZee <samabd9855@gmail.com>
2025-08-11 22:13:33 +0500

    TvT

---
+++ puzzle-05.py
@@ -0,0 +1,13 @@
+class Cat(object):
+    def __init__(self):
+        pass
+
+    def speak(self):
+        pass
+
+class student():
+    def __init__(self, name, id, grade):
+        self.name = name
+        self.id = id
+        self.grade = grade
+
