398413705733f9a3f8c1755339499a832385e61b
AbdullahZee <samabd9855@gmail.com>
2025-08-15 22:06:35 +0500

    puzzle 5

--- puzzle-05.py
+++ puzzle-05.py
@@ -1,13 +1,26 @@
-class Cat(object):
-    def __init__(self):
-        pass
-
-    def speak(self):
-        pass
-
 class student():
-    def __init__(self, name, id, grade):
+    def __init__(self, name, age, id,):
         self.name = name
+        self.age = age
         self.id = id
-        self.grade = grade
 
+students = {
+    'student1' : {
+        'name' : 'Albert',
+        'age' : 16,
+        'ID' : 1234567890
+    },
+    'student2' : {
+        'name' : 'Sam',
+        'age' : 15,
+        'ID' : 1234567890
+    },
+    'student3' : {
+        'name' : 'Robert',
+        'age' : 17,
+        'ID' : 1234567890
+    }
+}
+
+for y in students.values():
+    student(y['name'],y['age'],y['ID'])
