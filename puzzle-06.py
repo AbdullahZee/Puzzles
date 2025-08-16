862888f1cb8df1df4958d8fff60a9ad0564d3087
AbdullahZee <samabd9855@gmail.com>
2025-08-16 23:12:13 +0500

    puzzle 6

---
+++ puzzle-06.py
@@ -0,0 +1,42 @@
+block = [1,2,2]
+
+while 1:
+    print(block)
+    hit = input("What's hit? ")
+
+    if int(hit) == 1:
+        if block[0] % 4  != 0:
+            block[0] += 1
+        else:
+            block[0] = 1
+
+        if block[2] % 4  != 0:
+            block[2] += 1
+        else:
+            block[2] = 1
+
+    if int(hit) == 2:
+        if block[0] % 4  != 0:
+            block[0] += 1
+        else:
+            block[0] = 1
+
+        if block[1] % 4 != 0:
+            block[1] += 1
+        else:
+            block[1] = 1
+
+    if int(hit) == 3:
+        if block[1] % 4  != 0:
+            block[1] += 1
+        else:
+            block[1] = 1
+
+        if block[2] % 4 != 0:
+            block[2] += 1
+        else:
+            block[2] = 1
+
+    if block[0] == block[1] == block[2]:
+        break
+print("You won!!!")
